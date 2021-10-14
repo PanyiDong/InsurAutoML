be796889a6c56e3bbf437ae2171ca87901c2c61e
Panyi Dong <panyidong0@gmail.com>
2021-10-13 23:07:21 -0500

    modified feature selection from autosklearn

--- _feature_selection.py
+++ _feature_selection.py
@@ -24,13 +24,264 @@ from ._utils import nan_cov, maxloc, empirical_covariance, _class_means, _class_
 from autosklearn.pipeline.components.feature_preprocessing.densifier import Densifier
 from autosklearn.pipeline.components.feature_preprocessing.extra_trees_preproc_for_classification import ExtraTreesPreprocessorClassification
 from autosklearn.pipeline.components.feature_preprocessing.extra_trees_preproc_for_regression import ExtraTreesPreprocessorRegression
-from autosklearn.pipeline.components.feature_preprocessing.fast_ica import FastICA
-from autosklearn.pipeline.components.feature_preprocessing.feature_agglomeration import FeatureAgglomeration
-from autosklearn.pipeline.components.feature_preprocessing.kernel_pca import KernelPCA
-from autosklearn.pipeline.components.feature_preprocessing.kitchen_sinks import RandomKitchenSinks
 
 ######################################################################################################################
 # Modified Feature Selection from autosklearn
+
+class FastICA() :
+
+    '''
+    from autosklearn.pipeline.components.feature_preprocessing.fast_ica import FastICA
+    using 
+
+    Parameters
+    ----------
+    algorithm: Apply parallel or deflational algorithm, default = 'parallel'
+    supported ('parallel', 'deflation')
+
+    whiten: If false, no whitening is performed, default = True
+
+    fun: Functional form of the G function used, default = 'logcosh'
+    supported ('logcosh', 'exp', 'cube') or callable
+
+    n_components: Number of components to retain, default = None
+
+    seed: random seed, default = 1
+    '''
+
+    def __init__(
+        self, 
+        algorithm = 'parallel', 
+        whiten = True, 
+        fun = 'logcosh', 
+        n_components = None,
+        seed = 1
+    ) :
+        self.algorithm = algorithm
+        self.whiten = whiten
+        self.fun = fun
+        self.n_components = n_components
+        self.seed = seed
+
+        self.preprocessor = None
+
+    def fit(self, X, y=None):
+
+        import sklearn.decomposition
+
+        self.n_components = int(self.n_components)
+
+        self.preprocessor = sklearn.decomposition.FastICA(
+            n_components = self.n_components, algorithm = self.algorithm, fun = self.fun, \
+            whiten = self.whiten, random_state = self.seed
+        )
+        
+        # Make the RuntimeWarning an Exception!
+        with warnings.catch_warnings():
+            warnings.filterwarnings("error", message='array must not contain infs or NaNs')
+            try:
+                self.preprocessor.fit(X)
+            except ValueError as e:
+                if 'array must not contain infs or NaNs' in e.args[0]:
+                    raise ValueError("Bug in scikit-learn: "
+                                     "https://github.com/scikit-learn/scikit-learn/pull/2738")
+
+        return self
+
+    def transform(self, X):
+
+        if self.preprocessor is None:
+            raise NotImplementedError()
+
+        return self.preprocessor.transform(X)
+
+class FeatureAgglomeration() :
+
+    '''
+    from autosklearn.pipeline.components.feature_preprocessing.feature_agglomeration import FeatureAgglomeration
+    using 
+
+    Parameters
+    ----------
+    n_clusters: Number of clusters, default = 2
+        
+    affinity: Metric used to compute the linkage, default = 'euclidean'
+    supported ("euclidean", "l1", "l2", "manhattan", "cosine", or 'precomputed')
+        
+    linkage: Linkage criterion, default = 'ward'
+    supported ("ward", "complete", "average", "single")
+
+    pooling_func: Combines the values of agglomerated features into a single value, default = np.mean
+    
+    seed: random seed, default = 1
+    '''
+
+    def __init__(
+        self, 
+        n_clusters = 2, 
+        affinity = 'euclidean', 
+        linkage = 'ward', 
+        pooling_func = np.mean,
+        seed = 1
+    ) :
+        self.n_clusters = n_clusters
+        self.affinity = affinity
+        self.linkage = linkage
+        self.pooling_func = pooling_func
+        self.seed = seed
+
+        self.pooling_func_mapping = dict(mean = np.mean, median = np.median, max = np.max)
+
+        self.preprocessor = None
+
+    def fit(self, X, y = None) :
+
+        import sklearn.cluster
+
+        self.n_clusters = int(self.n_clusters)
+
+        n_clusters = min(self.n_clusters, X.shape[1])
+
+        if not callable(self.pooling_func):
+            self.pooling_func = self.pooling_func_mapping[self.pooling_func]
+
+        self.preprocessor = sklearn.cluster.FeatureAgglomeration(
+            n_clusters = n_clusters, affinity = self.affinity, linkage = self.linkage, pooling_func = self.pooling_func
+        )
+        self.preprocessor.fit(X)
+
+        return self
+
+    def transform(self, X):
+
+        if self.preprocessor is None:
+            raise NotImplementedError()
+
+        return self.preprocessor.transform(X)
+
+class KernelPCA() :
+
+    '''
+    from autosklearn.pipeline.components.feature_preprocessing.kernel_pca import KernelPCA
+    using sklearn.decomposition.KernelPCA
+
+    Parameters
+    ----------
+    n_components: number of features to retain, default = None
+
+    kernel: kernel used, default = 'linear'
+    supported (linear', 'poly', 'rbf', 'sigmoid', 'cosine', 'precomputed')
+
+    degree: Degree for poly kernels, default = 3
+
+    gamma: Kernel coefficient, default = 0.25
+
+    coef0: Independent term in poly and sigmoid kernels, default = 0.0
+
+    seed: random seed, default = 1
+    '''
+
+    def __init__(
+        self, 
+        n_components = None, 
+        kernel = 'linear', 
+        degree = 3, 
+        gamma = 0.25, 
+        coef0 = 0.0,
+        seed = 1
+    ):
+        self.n_components = n_components
+        self.kernel = kernel
+        self.degree = degree
+        self.gamma = gamma
+        self.coef0 = coef0
+        self.seed = seed
+
+        self.preprocessor = None
+
+    def fit(self, X, y = None) :
+
+        import sklearn.decomposition
+
+        self.n_components = int(self.n_components)
+        self.degree = int(self.degree)
+        self.gamma = float(self.gamma)
+        self.coef0 = float(self.coef0)
+
+        self.preprocessor = sklearn.decomposition.KernelPCA(
+            n_components = self.n_components, kernel = self.kernel, degree = self.degree, gamma = self.gamma, \
+            coef0 = self.coef0, remove_zero_eig = True, random_state = self.seed)
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("error")
+            self.preprocessor.fit(X)
+
+        if len(self.preprocessor.alphas_ / self.preprocessor.lambdas_) == 0:
+            raise ValueError('All features removed.')
+
+        return self
+
+    def transform(self, X) :
+
+        if self.preprocessor is None:
+            raise NotImplementedError()
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("error")
+
+            _X = self.preprocessor.transform(X)
+
+            if _X.shape[1] == 0:
+                raise ValueError("KernelPCA removed all features!")
+
+            return _X
+
+class RandomKitchenSinks() :
+
+    '''
+    from autosklearn.pipeline.components.feature_preprocessing.kitchen_sinks import RandomKitchenSinks
+    using sklearn.kernel_approximation.RBFSampler
+
+    Parameters
+    ----------
+    gamma: use to determine standard variance of random weight table, default = 1
+    Parameter of RBF kernel: exp(-gamma * x^2).
+
+    n_components: number of samples per original feature, default = 100
+
+    seed: random seed, default = 1
+    '''
+
+    def __init__(
+        self,
+        gamma = 1.0,
+        n_components = 100,
+        seed = 1
+    ) :
+        self.gamma = gamma
+        self.n_components = n_components
+        self.seed = seed
+
+        self.preprocessor = None
+
+    def fit(self, X, y = None) :
+
+        import sklearn.kernel_approximation
+
+        self.n_components = int(self.n_components)
+        self.gamma = float(self.gamma)
+
+        self.preprocessor = sklearn.kernel_approximation.RBFSampler(self.gamma, self.n_components, self.seed)
+        self.preprocessor.fit(X)
+
+        return self
+
+    def transform(self, X) :
+
+        if self.preprocessor is None:
+            raise NotImplementedError()
+
+        return self.preprocessor.transform(X)
 
 class LibLinear_Preprocessor() :
 
