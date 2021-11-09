660816ef8568ce4da7495f9c67c882ee4c0c887e
Panyi Dong <panyidong0@gmail.com>
2021-11-08 20:16:49 -0600

    reformat MLP

--- _model.py
+++ _model.py
@@ -2,8 +2,8 @@ from cProfile import label
 import numpy as np
 import pandas as pd
 import torch
-import torch.optim
-from torch import nn
+import torch.nn as nn
+from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 
@@ -12,6 +12,8 @@ import warnings
 from ._utils import type_of_task
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # check if gpu available
+
+#############################################################################################
 
 #############################################################################################
 # Linear Neural Network (LNN)
@@ -68,11 +70,11 @@ class LNN :
 
     def fit(self, X, y) :
 
-        # initialize forward steps
-        self._model = _LNN(inputSize = self.inputSize, outputSize = self.outputSize).to(device)
-
         if self.inputSize != X.shape[1] :
             self.inputSize = X.shape[1]
+
+        # initialize forward steps
+        self._model = _LNN(inputSize = self.inputSize, outputSize = self.outputSize).to(device)
 
         # converting inputs to pytorch tensors
         if isinstance(X, np.ndarray) :
@@ -128,7 +130,7 @@ class LNN :
 #############################################################################################
 # Multilayer Perceptrons (MLP)
 # Forward step
-class MLP(pl.LightningModule) :
+class _MLP(nn.Module) :
 
     def __init__(
         self,
@@ -151,12 +153,99 @@ class MLP(pl.LightningModule) :
 
         return self.layers(X)
 
-    def training_step(self, batch, batch_idx) :
+# Optimization step
+class MLP :
+    
+    '''
+    inputSize:
+    
+    outputSize:
 
-        x, y = batch
-        output = self(x)
-        loss = nn.CrossEntropyLoss(output, y)
-        return loss
+    criteria: loss function, default = 'MSE'
 
-    def configure_optimizers(self) :
-        return torch.optim.SGD(self.parameters(), lr = self.learning_rate)
+    optimizer: optimizer used for backpropagation, default = 'SGD'
+
+    learning_rate: learning rate for gradient descent, default = 0.03
+
+    max_iter: maximum iterations allowed, default = 1000
+    '''
+
+    def __init__(
+        self, 
+        inputSize = 1, 
+        hiddenSize = 5,
+        outputSize = 1,
+        criteria = 'MSE',
+        optimizer = 'SGD',
+        learning_rate = 0.03,
+        max_iter = 1000
+    ):
+        self.inputSize = inputSize
+        self.hiddenSize = hiddenSize
+        self.outputSize = outputSize
+        self.criteria = criteria
+        self.optimizer = optimizer
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
+
+    def fit(self, X, y) :
+
+        if self.inputSize != X.shape[1] :
+            self.inputSize = X.shape[1]
+
+        # initialize forward steps
+        self._model = _MLP(
+            inputSize = self.inputSize, hiddenSize = self.hiddenSize, outputSize = self.outputSize
+        ).to(device)
+
+        # converting inputs to pytorch tensors
+        if isinstance(X, np.ndarray) :
+            inputs = torch.from_numpy(X).to(device)
+        elif isinstance(X, pd.DataFrame) :
+            inputs = torch.from_numpy(X.values).to(device)
+
+        if isinstance(y, np.ndarray) :
+            labels = torch.from_numpy(y).reshape(-1, 1).to(device)
+        elif isinstance(y, pd.DataFrame) :
+            labels = torch.from_numpy(y.values).reshape(-1, 1).to(device)
+
+        # define the loss function and optimizer
+        if self.criteria == 'MSE' :
+            criterion = nn.MSELoss()
+
+        if self.optimizer == 'SGD' :
+            optimizer = torch.optim.SGD(self._model.parameters(), lr = self.learning_rate)
+
+        Losses = [] # record losses for early stopping or 
+
+        for _ in range(self.max_iter) :
+    
+            # clear gradient buffers
+            optimizer.zero_grad()
+
+            # calculate output using inputs
+            outputs = self._model(inputs)
+
+            # get loss from outputs
+            loss = criterion(outputs, labels)
+            Losses.append(loss.item())
+
+            # get gradient for parameters
+            loss.backward()
+
+            # update parameters
+            optimizer.step()
+
+    def predict(self, X) :
+
+        # converting inputs to pytorch tensors
+        if isinstance(X, np.ndarray) :
+            inputs = torch.from_numpy(X).to(device)
+        elif isinstance(X, pd.DataFrame) :
+            inputs = torch.from_numpy(X.values).to(device)
+
+        with torch.no_grad() :
+
+            return self._model(inputs).detach().cpu().numpy() # convert to numpy
+
+#############################################################################################
