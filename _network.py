6631c9f1b695c26c83afc374feadbfa65776b8f2
Panyi Dong <panyidong0@gmail.com>
2021-11-10 21:41:48 -0600

    tested LeNet

--- _network.py
+++ _network.py
@@ -1,3 +1,4 @@
+from typing import ForwardRef
 import numpy as np
 import pandas as pd
 import torch
@@ -28,6 +29,14 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # check if
 #     if type(m) == nn.Linear :
 #         nn.init.xavier_uniform_(m.weight) # Xavier initializer
 
+# convert numpy array to pytorch tensor
+def to_tensor(X, y, batch_size = 10) :
+
+    inputs = torch.tensor(X).to(device)
+    labels = torch.tensor(y).to(device)
+
+    return DataLoader(torch.cat([inputs, labels], dim = 1), batch_size = batch_size, shuffle = True)
+
 #############################################################################################
 # Common layers
 
@@ -56,6 +65,27 @@ class LinearLayer(nn.Module) :
 
         output = torch.matmul(X, self.weight.data) + self.bias.data
         return output
+
+# convolutional layer
+# 2D Cross-correlation operation
+def corr2d(X, K) :
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]) :
+        for j in range(Y.shape[1]) :
+            Y[i, j] = (X[i:i + h, j:j + w] * K).sum()
+
+    return Y
+
+class Conv2D(nn.Module) :
+
+    def __init__(self, kernel_size) :
+        super().__init__()
+        self.weight = nn.Parameter(torch.rand(kernel_size)) # random weight
+        self.bias = nn.Parameter(torch.zeros(1)) # zero bias
+
+    def forward(self, X) :
+        return corr2d(X, self.weight) + self.bias
 
 #############################################################################################
 # Linear Neural Network (LNN)
@@ -295,9 +325,71 @@ class MLP :
 
 #############################################################################################
 # Convolutional Neural Network (CNN)
+
 # Ideas: Translation Invariance and Locality
 # 1. Translation Invariance: Network should respond similarily to patches
 # 2. Locality: Network should focus on local regions
-# Forward Step
 
-# Optimization Step
+# Padding: create surrounding empty values to increase the inputsize so reduction will be smaller 
+# or even retain the inputsize, the boundaries will be preserved
+# Stride: skip intermediate locations to improve efficiency or the purpose of downsample
+
+# Max Pooling, Average Pooling
+
+# LeNet-5
+# Layers: Two Convolutional blocks, follow by three fully connected linear layers
+# each Convolutional block consists of Convolutional layer, Activation function, AveragePool layer
+class LeNet(nn.Module) :
+
+    def __init__(
+        self,
+        Conv_inChannel = (1, 6),
+        Conv_outChannel = (6, 16),
+        Conv_kernel = (5, 5),
+        Conv_padding = (2, 0),
+        Conv_stride = (1, 1),
+        Pool_kernel = (2, 2),
+        Pool_padding = (0, 0),
+        Pool_stride = (2, 2),
+        denseSize = (120, 84, 10),
+    ) :
+        super().__init__()
+        self.Con2d1 = nn.Conv2d(
+            Conv_inChannel[0], Conv_outChannel[0], kernel_size = Conv_kernel[0],
+            padding = Conv_padding[0], stride = Conv_stride[0]
+        )
+        self.Con2d2 = nn.Conv2d(
+            Conv_inChannel[1], Conv_outChannel[1], kernel_size = Conv_kernel[1], 
+            padding = Conv_padding[1], stride = Conv_stride[1]
+        )
+        self.AvgPool2d1 = nn.AvgPool2d(
+            kernel_size = Pool_kernel[0], padding = Pool_padding[0], stride = Pool_stride[0]
+        )
+        self.AvgPool2d2 = nn.AvgPool2d(
+            kernel_size = Pool_kernel[1], padding = Pool_padding[1], stride = Pool_stride[1]
+        )
+        self.Sigmoid = nn.Sigmoid()
+
+        self.Conv_block = nn.Sequential(
+            self.Con2d1, self.Sigmoid, self.AvgPool2d1, # first Convolution block
+            self.Con2d2, self.Sigmoid, self.AvgPool2d2, # second Convolution block
+            nn.Flatten() # flatten for linear layer
+        )
+
+        self.denseSize = denseSize # mark the size of linear, self.Linear1 requires change of size
+        self.Linear1 = nn.Linear(120, denseSize[0])
+        self.Linear2 = nn.Linear(denseSize[0], denseSize[1])
+        self.Linear3 = nn.Linear(denseSize[1], denseSize[2])
+
+    def forward(self, X) :
+        
+        # Convolution block
+        output = self.Conv_block(X)
+
+        # three fully-connected linear layers
+        self.Linear1 = nn.Linear(output.size(dim = 1), self.denseSize[0])
+        output = self.Linear1(output)
+        output = self.Linear2(output)
+        output = self.Linear3(output)
+
+        return output
