ac01a20c5a15596dbecd8435a218e3ebdc62dfa1
Panyi Dong <panyidong0@gmail.com>
2021-11-13 19:21:13 -0600

    add Vocabulary

--- _network.py
+++ _network.py
@@ -1,4 +1,4 @@
-from typing import ForwardRef
+import collections
 import numpy as np
 import pandas as pd
 from pandas.core.algorithms import isin
@@ -46,7 +46,64 @@ def to_tensor(X, y, batch_size = 10) :
 
     return DataLoader(torch_dataset, batch_size = batch_size, shuffle = True)
 
+# count token frequencies
+def count_corpus(tokens) :
+
+    if len(tokens) == 0 or isinstance(tokens[0], list) :
+        tokens = [token for line in tokens for token in line]
+
+    return collections.Counter(tokens)
+
 # create vocabulary for text tokens
+class Vocab :
+
+    def __init__(
+        self,
+        tokens = None,
+        min_freq = 0,
+        reserved_tokens = None
+    ) :
+        if tokens is None :
+            tokens = []
+        if reserved_tokens is None :
+            reserved_tokens = []
+
+        # Sort text tokens by frequencies
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key = lambda x: x[1], reserve = True)
+
+        # store unknown tokens at index 0
+        self.idx_to_token = ['<unk>'] + reserved_tokens
+        self.token_to_idx = {
+            token: idx for idx, token in enumerate(self.idx_to_token)
+        }
+        for token, freq in self._token_freqs :
+            if freq < min_freq :
+                break
+            if token not in self.token_to_idx :
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self) :
+        return len(self.idx_to_token)
+    
+    def __getitem__(self, tokens) :
+        if not isinstance(tokens, (list, tuple)) :
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices) :
+        if not isinstance(indices, (list, tuple)) :
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self) :
+        return 0
+
+    @property
+    def token_freqs(self) :
+        return self._token_freqs
 
 #############################################################################################
 # Common layers
