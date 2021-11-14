09339d7ea3646e53e8b92ad558cfbf4708f4434f
Panyi Dong <panyidong0@gmail.com>
2021-11-13 22:56:15 -0600

    add example of RNN

--- _network.py
+++ _network.py
@@ -851,4 +851,51 @@ class NiN(nn.Module) :
 # DenseNet (Densely Connected Networks)
 
 #############################################################################################
-# Recurrent Neural Network (RNN) 
+# Recurrent Neural Network (RNN)
+class RNN(nn.Module):
+
+    '''
+    one RNN layer + fully-connected linear layer
+    '''
+
+    def __init__(
+        self, 
+        input_size, 
+        output_size, 
+        hidden_dim, 
+        n_layers
+    ):
+        super(RNN, self).__init__()
+
+        # Defining some parameters
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        #Defining the layers
+        # RNN Layer
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)   
+        # Fully connected layer
+        self.linear = nn.Linear(hidden_dim, output_size)
+    
+    def forward(self, x):
+        
+        batch_size = x.size(0)
+
+        # Initializing hidden state for first input using method defined below
+        hidden = self.init_hidden(batch_size)
+        
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, hidden = self.rnn(x, hidden)
+        
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        out = nn.Flatten(out)
+        out = self.linear(out)
+        
+        return out, hidden
+    
+    def init_hidden(self, batch_size):
+
+        # This method generates the first hidden state of zeros which we'll use in the forward pass
+        # We'll send the tensor holding the hidden state to the device we specified earlier as well
+        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
+        return hidden
