a3f25eb9b6818643a962f4a78026ec1dcc3e8472
Panyi Dong <panyidong0@gmail.com>
2022-03-01 00:22:35 -0600

    update header

--- My_AutoML/_balancing.py
+++ My_AutoML/_balancing.py
@@ -7,12 +7,31 @@ Project: My_AutoML
 Relative Path: /_balancing.py
 File Created: Friday, 25th February 2022 6:13:42 pm
 Author: Panyi Dong (panyid2@illinois.edu)
+
 -----
-Last Modified: Tuesday, 1st March 2022 12:08:03 am
-Modified By: Panyi Dong (panyid2@illinois.edu>)
+Last Modified: Tuesday, 1st March 2022 12:18:54 am
+Modified By: Panyi Dong (panyid2@illinois.edu)
+
 -----
+MIT License
+
 Copyright (c) 2022 - 2022, Panyi Dong
-All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 
 import numpy as np
